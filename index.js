f68bc3e979e880f22c1fe57b6e6270528138d9de
Nicolas Orjuela <n.orjuela@uniandes.edu.co>
2022-03-03 13:06:25 -0500

    ejercicio nodo 201921070

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const fs = require('fs');
-const items = null;
+var list = null;
+var newlist = [];
 
 fs.readFile('products-ecommerce.json', 'utf-8', (err, data) => {
-  items = data;
+  list = JSON.parse(data);
+  transform(list);
+
+  fs.writeFile('ejercicionodo.json', newlist, (err) => {
+    if (err) throw err;
+    console.log('Data written to file');
+  });
+
 })
+
+function transform(lista)
+{
+  lista.items.forEach(element => {
+    if(element.price.amount < 10000) newlist.push(element);    
+  });
+  newlist = JSON.stringify(newlist, null, 2);
+}
+
+
